a873d4b378dd38a2e6bd690b2c128b2f5ce531b9
Devphani6 <Devphani236@gmail.com>
2023-01-12 19:07:15 +0530

    Math operands

---
+++ progonsimpleinterest.py
@@ -0,0 +1,11 @@
+# Write a python program to print simple interest
+# here p for principle Amount
+#     T for Time
+#     R for Rate of interest
+P=float(input('Enter principle amount : '))
+T=float(input('Enter Time value : '))
+R=float(input('Rate of interest : '))
+onemonthint=(P * R) /100
+print('One month interest : ', onemonthint)
+si=(P * T * R) /100
+print('Simple Interest', si)
