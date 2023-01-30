6333fd3eda4598db0920b8a508a51a8d34202f32
Devphani6 <Devphani236@gmail.com>
2023-01-30 19:23:04 +0530

    comit a if else python program

---
+++ if-else.py
@@ -0,0 +1,17 @@
+age=int(input('Enter your age :'))
+if age<=1:
+  print('Just Born')
+elif age>=1 and age<=12:
+    print('Childish')
+elif age>=13 and age<=19:
+    print('Teenage')
+elif age>=20 and age<=30:
+    print('Youngage')
+elif age>=31 and age<=50:
+    print('Middle age')
+elif age>=51 and age<75:
+    print('Senior Citizen')
+elif age>=75:
+    print('Senior Most Citizen')
+
+
